42bcaf6dcadb04aadf19d44461bcfcf0bbd39007
alexusbernard17 <alexusbernardbrooklyn@gmail.com>
2025-03-15 23:39:49 -0400

    fix header links styling for mobile view

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,16 +10,16 @@ const Home = () => {
         <div >
             <div className='HomePage'>
                 <div className='HomeText'>
-                <h1>Hello World 👋,</h1>
-                <h1>I'm <b>Alexus Brooklyn</b></h1>
-                <Typed/>
+                    <h1>Hello World 👋,</h1>
+                    <h1>I'm <b>Alexus Brooklyn</b></h1>
+                    <Typed/>
 
-                <div className='headerLinks'>
-                    <a href="https://github.com/alexusbernard17/" target='_blank' rel="noopener noreferrer"><FaGithub/></a>
-                    <a href="https://linkedin.com/in/alexusbrooklyn/" target='_blank' rel="noopener noreferrer"><FaLinkedin/></a>
-                    <a href='mailTo:alexusbernardbrooklyn@gmail.com' target='_blank' rel="noopener noreferrer"><IoMail/></a>
-                    <a href='https://instagram.com/alexusbrooklyn/' target='_blank' rel="noopener noreferrer"><FaInstagram/></a>
-                </div>
+                    <div className='headerLinks'>
+                        <a href="https://github.com/alexusbernard17/" target='_blank' rel="noopener noreferrer"><FaGithub/></a>
+                        <a href="https://linkedin.com/in/alexusbrooklyn/" target='_blank' rel="noopener noreferrer"><FaLinkedin/></a>
+                        <a href='mailTo:alexusbernardbrooklyn@gmail.com' target='_blank' rel="noopener noreferrer"><IoMail/></a>
+                        <a href='https://instagram.com/alexusbrooklyn/' target='_blank' rel="noopener noreferrer"><FaInstagram/></a>
+                    </div>
                 </div>
 
                 <Lottie 
